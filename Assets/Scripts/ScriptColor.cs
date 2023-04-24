ea0ef2f6671884d36dc51f6d3228acd5b44d920d
GuilledlC <guillermodelacalhacar@gmail.com>
2023-04-25 00:55:46 +0200

    Mejorado los colores de los ladrillos

--- Assets/Scripts/ScriptColor.cs
+++ Assets/Scripts/ScriptColor.cs
@@ -9,10 +9,14 @@ public class ScriptColor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Renderer>().material.color = new Color(
-            Random.Range(0.6f, 1.0f),
-            Random.Range(0.3f, 0.7f),
-            Random.Range(0.0f, 0.4f));
+        GetComponent<Renderer>().material.color =
+            Color.Lerp(
+            Color.gray, 
+            Color.Lerp(
+                Color.red,
+                Color.yellow,
+                Random.Range(0.25f, 0.70f)),
+            Random.Range(0.2f, 0.6f));
     }
 
     // Update is called once per frame
