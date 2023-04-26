24984ecb88ccfe962d546707e66ae948aba31878
alephandro <alex29561@gmail.com>
2023-04-26 12:44:55 +0200

    Primera version sistema de guardado menu opciones

--- Assets/Scripts/Menu/MostrarValorPorcentaje.cs
+++ Assets/Scripts/Menu/MostrarValorPorcentaje.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MostrarValorPorcentaje : MonoBehaviour
 {
     private TextMeshProUGUI porcentaje;
-
+    public TowerStats towerStats;
     void Start()
     {
         porcentaje = this.GetComponent<TextMeshProUGUI>();
@@ -15,5 +15,11 @@ public class MostrarValorPorcentaje : MonoBehaviour
     public void ActualizarTexto(float valor)
     {
         porcentaje.text = Mathf.RoundToInt(valor * 100) + "%";
+    }
+
+    public void guardarEstadisticas()
+    {
+        towerStats.setPorcentajeDestuccion(int.Parse(porcentaje.text.Trim('%')));
+        print(towerStats.getPorcentajeDestuccion());
     }
 }
